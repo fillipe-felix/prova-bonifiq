c57bf501f5d1296b1b88d8822160b48c2a9dbc88
fillipe.felix <felipesoares_1993@hotmail.com>
2025-03-10 20:02:54 -0300

    Parte 1
    Movido a inicialização de seed dentro do método GetRandom para garantir geração de seed única por chamada. Além disso, substituído SaveChanges por SaveChangesAsync para melhor manipulação de operação assíncrona.

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -6,22 +6,21 @@ namespace ProvaPub.Services
 {
 	public class RandomService
 	{
-		int seed;
-        TestDbContext _ctx;
+        private readonly TestDbContext _ctx;
 		public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
-            seed = Guid.NewGuid().GetHashCode();
-
+	            .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+	            .Options;
+            
             _ctx = new TestDbContext(contextOptions);
         }
         public async Task<int> GetRandom()
-		{
+        {
+	        var seed = Guid.NewGuid().GetHashCode();
             var number =  new Random(seed).Next(100);
             _ctx.Numbers.Add(new RandomNumber() { Number = number });
-            _ctx.SaveChanges();
+            await _ctx.SaveChangesAsync();
 			return number;
 		}
 
