eec7065e0700c5afb8ebec72e962050ee45f4248
fillipe.felix <felipesoares_1993@hotmail.com>
2025-03-11 12:18:24 -0300

    Refatoração da lógica de validação de compra do cliente.
    Extraia métodos auxiliares para melhorar a legibilidade e a manutenibilidade do código. Encapsule regras de negócios em métodos dedicados para melhor modularidade e clareza. Isso reduz a duplicação e centraliza as verificações relacionadas a data/hora.

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -40,20 +40,39 @@ public class CustomerService : PagedListService, ICustomerService
         if (customer == null) throw new InvalidOperationException($"Customer Id {customerId} does not exists");
 
         //Business Rule: A customer can purchase only a single time per month
-        var baseDate = _dateTime.UtcNow.AddMonths(-1);
-        var ordersInThisMonth = await _customerRepository.HasPurchasedInLastMonthAsync(customerId, baseDate);
-        if (ordersInThisMonth)
+        var currentDateTime = _dateTime.UtcNow;
+        if (await HasPurchasedInLastMonth(customerId, currentDateTime))
             return false;
 
         //Business Rule: A customer that never bought before can make a first purchase of maximum 100,00
-        var haveBoughtBefore = await _customerRepository.HasEverPurchasedAsync(customerId);
-        if (haveBoughtBefore && purchaseValue > 100)
+        if (!await HasMadePreviousPurchase(customerId) && purchaseValue > 100)
             return false;
 
         //Business Rule: A customer can purchases only during business hours and working days
-        if (_dateTime.UtcNow.Hour < 8 || _dateTime.UtcNow.Hour > 18 || _dateTime.UtcNow.DayOfWeek == DayOfWeek.Saturday || _dateTime.UtcNow.DayOfWeek == DayOfWeek.Sunday)
+        if (IsOutsideBusinessHours(_dateTime.UtcNow) || IsWeekend(_dateTime.UtcNow))
             return false;
 
         return true;
+    }
+    
+    private bool IsOutsideBusinessHours(DateTime dateTime)
+    {
+        return dateTime.Hour < 8 || dateTime.Hour > 18;
+    }
+
+    private bool IsWeekend(DateTime dateTime)
+    {
+        return dateTime.DayOfWeek == DayOfWeek.Saturday || dateTime.DayOfWeek == DayOfWeek.Sunday;
+    }
+    
+    private async Task<bool> HasPurchasedInLastMonth(int customerId, DateTime currentDateTime)
+    {
+        var oneMonthAgo = currentDateTime.AddMonths(-1);
+        return await _customerRepository.HasPurchasedInLastMonthAsync(customerId, oneMonthAgo);
+    }
+
+    private async Task<bool> HasMadePreviousPurchase(int customerId)
+    {
+        return await _customerRepository.HasEverPurchasedAsync(customerId);
     }
 }
